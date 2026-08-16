7309777dba0169430d149a11723b36303b654097
Noufal Rafsanjani <muhammad.noufal.rafsan13@gmail.com>
2026-08-16 14:37:06 +0700

    fix: prevent SQL injection in login and register using prepared statements

--- proses_register.php
+++ proses_register.php
@@ -1,13 +1,16 @@
 <?php
 session_start();
+
 include "koneksi.php";
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-$query = "INSERT INTO users (email, password) VALUES ('$email', '$hashed_password')";
-mysqli_query($conn, $query);
+$query = "INSERT INTO users (email, password) VALUES (?, ?)";
+$stmt = mysqli_prepare($conn, $query);
+mysqli_stmt_bind_param($stmt, "ss", $email, $hashed_password);
+mysqli_stmt_execute($stmt);
 
 header("Location: login.php");
 exit();
